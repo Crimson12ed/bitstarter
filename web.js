5f6df7d283bbd62cb86e484cf1079759c6614316
Alex Ng <Alex@Lenovo.(none)>
2013-07-15 22:22:44 -0700

    Updated index.html, web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var outfile = "index.html";
+var outfile = "indexnew.html";
 var buffer = new Buffer(fs.readFileSync(outfile), "utf-8");
 var output = buffer.toString();
 
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
     response.send(output);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
